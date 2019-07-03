3d218a2e8782ea8ecd86032b08f547c9e43367d3
bharathck <bharathchandra541@gmail.com>
2019-07-03 15:57:06 +0530

    add modal for entering the description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,35 @@ export class AppComponent {
 
   articles: Article[];
 
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+  dummyArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
 
     console.log(`adding article title : ${title.value} and link : ${link.value}`);
+    
+    return false;
+  }
 
-    this.articles.push(new Article(title.value ,link.value , 0));
+  addDesc(title: HTMLInputElement, link: HTMLInputElement, desc: HTMLInputElement): boolean{
+    console.log(`ARTICLE TITLE,LINK & DESC : ${title.value} , ${link.value} , ${desc.value}`);
+
+    this.articles.push(new Article(title.value, link.value , desc.value, 0));
     title.value = '';
     link.value = '';
     return false;
   }
 
+  closeModal(title: HTMLInputElement, link: HTMLInputElement, desc: HTMLInputElement): boolean{
+
+    title.value = '';
+    link.value = '';
+
+    return false;
+  }
+
   constructor(){
     this.articles=[
-      new Article('Best Version control Software','https://github.com',10),
-      new Article('Best mobile Phone Under 30k','https://oneplus.com',12),
-      new Article('Gst rate changes?','https://hindu-news.com',4)
+      new Article('Best Version control Software','https://github.com','GitHub brings together the worlds largest community of developers to discover, share, and build better software. From open source projects to private team ...',10),
+      new Article('Best mobile Phone Under 30k','https://oneplus.com','OnePlus creates beautifully designed products with premium build quality & brings the best technology to users around the world. No tradeoffs, we #NeverSettle.',12),
+      new Article('Gst rate changes?','https://hindu-news.com','NOTHING TO DESCRIBE',4)
       ];
   }
 
