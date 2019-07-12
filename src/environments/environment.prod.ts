35e0883fb4201b8f810caa59ad516e12ee1ba3db
bharathck <bharathchandra541@gmail.com>
2019-07-12 17:48:08 +0530

    Change firebase database
    Use my own realtime_firebase instead of the one got with source
    
    Signed-off-by: bharathck <bharathchandra541@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+
+/*
   firebase: {
     apiKey: 'xxx',
     authDomain: 'gkz-angular-firebase.firebaseapp.com',
@@ -7,5 +9,15 @@ export const environment = {
     projectId: 'gkz-angular-firebase',
     storageBucket: 'gkz-angular-firebase.appspot.com',
     messagingSenderId: '123'
+  }
+  */
+  firebase: {
+    apiKey: "AIzaSyBRnd1049UVsmyrNvbhBTW9D81XdVLSfDc",
+    authDomain: "feed-35645.firebaseapp.com",
+    databaseURL: "https://feed-35645.firebaseio.com",
+    projectId: "feed-35645",
+    storageBucket: "feed-35645.appspot.com/",
+    messagingSenderId: "539920875190",
+    appId: "1:539920875190:web:872f1c4248d97702"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: true,
+  /*
   firebase: {
     apiKey: 'xxx',
     authDomain: 'gkz-angular-firebase.firebaseapp.com',
@@ -11,6 +12,16 @@ export const environment = {
     projectId: 'gkz-angular-firebase',
     storageBucket: 'gkz-angular-firebase.appspot.com',
     messagingSenderId: '123'
+  }
+  */
+  firebase: {
+    apiKey: "AIzaSyBRnd1049UVsmyrNvbhBTW9D81XdVLSfDc",
+    authDomain: "feed-35645.firebaseapp.com",
+    databaseURL: "https://feed-35645.firebaseio.com",
+    projectId: "feed-35645",
+    storageBucket: "feed-35645.appspot.com/",
+    messagingSenderId: "539920875190",
+    appId: "1:539920875190:web:872f1c4248d97702"
   }
 };
 
